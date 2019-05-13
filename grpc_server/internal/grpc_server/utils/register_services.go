b999092a0e38d1eba29d406bbe96c79beb0ecf56
azharprabudi <azharprabui@gmail.com>
2019-05-13 13:27:19 +0700

    apply article presentation to grpc service

--- grpc_server/internal/grpc_server/utils/register_services.go
+++ grpc_server/internal/grpc_server/utils/register_services.go
@@ -1,6 +1,15 @@
 package utils
 
-import "google.golang.org/grpc"
+import (
+	presentation "github.com/azharprabudi/flutter_grpc_graphql/grpc_server/internal/grpc_server/presentation/articles"
+	"github.com/azharprabudi/flutter_grpc_graphql/grpc_server/pkg/api/v1/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
 
 func RegisterServices(server *grpc.Server) {
+	article := presentation.NewArticlePresentation()
+	proto.RegisterArticleServiceServer(server, article)
+
+	reflection.Register(server)
 }
