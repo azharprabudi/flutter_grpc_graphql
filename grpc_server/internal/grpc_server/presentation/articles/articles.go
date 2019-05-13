20d6574811f4e2e25ed8a817a43a1de1ff60ef12
azharprabudi <azharprabui@gmail.com>
2019-05-13 13:44:08 +0700

    renaming file

--- grpc_server/internal/grpc_server/presentation/articles/articles.go
+++ grpc_server/internal/grpc_server/presentation/articles/articles.go
@@ -3,13 +3,16 @@ package presentation
 import (
 	"context"
 
+	service "github.com/azharprabudi/flutter_grpc_graphql/grpc_server/internal/grpc_server/service/articles"
 	"github.com/azharprabudi/flutter_grpc_graphql/grpc_server/pkg/api/v1/proto"
 )
 
 type ArticlePresentation struct {
+	service service.ArticleServiceInterface
 }
 
 func (ap *ArticlePresentation) Get(context.Context, *proto.ArticlePagination) (*proto.Articles, error) {
+	// articles
 	return nil, nil
 }
 
@@ -18,5 +21,7 @@ func (ap *ArticlePresentation) Bookmark(context.Context, *proto.Article) (*proto
 }
 
 func NewArticlePresentation() proto.ArticleServiceServer {
-	return &ArticlePresentation{}
+	return &ArticlePresentation{
+		service: service.NewArticleService(),
+	}
 }
