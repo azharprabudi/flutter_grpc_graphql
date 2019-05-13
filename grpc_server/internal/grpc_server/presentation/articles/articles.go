0fe4ddd65624412dcb4418ca8ae5dc7cacc3d439
azharprabudi <azharprabui@gmail.com>
2019-05-13 13:50:31 +0700

    add get article presentation

--- grpc_server/internal/grpc_server/presentation/articles/articles.go
+++ grpc_server/internal/grpc_server/presentation/articles/articles.go
@@ -11,12 +11,17 @@ type ArticlePresentation struct {
 	service service.ArticleServiceInterface
 }
 
-func (ap *ArticlePresentation) Get(context.Context, *proto.ArticlePagination) (*proto.Articles, error) {
-	// articles
-	return nil, nil
+func (ap *ArticlePresentation) Get(ctx context.Context, req *proto.ArticlePagination) (*proto.Articles, error) {
+	page := int(req.Page)
+	resp, err := ap.service.Get(page)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
 
-func (ap *ArticlePresentation) Bookmark(context.Context, *proto.Article) (*proto.Empty, error) {
+func (ap *ArticlePresentation) Bookmark(ctx context.Context, req *proto.Article) (*proto.Empty, error) {
 	return nil, nil
 }
 
