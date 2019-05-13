0abdeb7169997d6dcee2a95a0c8fd7d5dffc668c
azharprabudi <azharprabui@gmail.com>
2019-05-13 13:50:38 +0700

    add db port

--- grpc_server/configs/configs.go
+++ grpc_server/configs/configs.go
@@ -2,6 +2,7 @@ package configs
 
 const (
 	PORT             = 6969
+	PORT_REDIS       = 6379
 	BASEURL_ARTICLES = "https://newsapi.org/v2"
 	API_KEY          = "c6f23162e2f8454f8a87763a5c9d7fca" // free api so i dont worry to throw this into it
 )
