bb17a257d024862c7eb582a00f2f271ff4508b59
azharprabudi <azharprabui@gmail.com>
2019-05-13 13:41:51 +0700

    add service to get articles

---
+++ grpc_server/internal/grpc_server/service/articles/articles.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/azharprabudi/flutter_grpc_graphql/grpc_server/configs"
+	"github.com/azharprabudi/flutter_grpc_graphql/grpc_server/pkg/api/v1/proto"
+	"github.com/payfazz/go-apt/pkg/fazzcloud"
+)
+
+type ArticleService struct {
+	http *fazzcloud.HTTPClient
+}
+
+type ArticleServiceInterface interface {
+	Get(page int) (*proto.Articles, error)
+}
+
+func (as *ArticleService) Get(page int) (*proto.Articles, error) {
+	_, resp, err := as.http.Get("/everything", &map[string]string{
+		"q":       "sport",
+		"country": "id",
+		"apiKey":  configs.API_KEY,
+		"page":    strconv.Itoa(page),
+	}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var articles *proto.Articles
+	if err = json.Unmarshal(resp, &articles); err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
+
+func NewArticleService() ArticleServiceInterface {
+	return &ArticleService{
+		http: fazzcloud.NewHTTPClient(configs.BASEURL_ARTICLES),
+	}
+}
