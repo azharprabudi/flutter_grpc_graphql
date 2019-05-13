2f3306ccc55d1d3a25ff33f5709f9aa558547e55
azharprabudi <azharprabui@gmail.com>
2019-05-13 13:20:51 +0700

    add pagination at get service and add bookmark article service

--- grpc_server/pkg/api/v1/proto/article.pb.go
+++ grpc_server/pkg/api/v1/proto/article.pb.go
@@ -4,8 +4,10 @@
 package proto
 
 import (
+	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
 	math "math"
 )
 
@@ -76,17 +78,17 @@ func (m *Articles) GetArticles() []*Article {
 }
 
 type Article struct {
-	Source               *Source  `protobuf:"bytes,1,opt,name=source,proto3" json:"source,omitempty"`
-	Author               string   `protobuf:"bytes,2,opt,name=author,proto3" json:"author,omitempty"`
-	Title                string   `protobuf:"bytes,3,opt,name=title,proto3" json:"title,omitempty"`
-	Description          string   `protobuf:"bytes,4,opt,name=description,proto3" json:"description,omitempty"`
-	Url                  string   `protobuf:"bytes,5,opt,name=url,proto3" json:"url,omitempty"`
-	UrlToImage           string   `protobuf:"bytes,6,opt,name=urlToImage,proto3" json:"urlToImage,omitempty"`
-	PublishedAt          string   `protobuf:"bytes,7,opt,name=publishedAt,proto3" json:"publishedAt,omitempty"`
-	Content              string   `protobuf:"bytes,8,opt,name=content,proto3" json:"content,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	Source               *ArticleSource `protobuf:"bytes,1,opt,name=source,proto3" json:"source,omitempty"`
+	Author               string         `protobuf:"bytes,2,opt,name=author,proto3" json:"author,omitempty"`
+	Title                string         `protobuf:"bytes,3,opt,name=title,proto3" json:"title,omitempty"`
+	Description          string         `protobuf:"bytes,4,opt,name=description,proto3" json:"description,omitempty"`
+	Url                  string         `protobuf:"bytes,5,opt,name=url,proto3" json:"url,omitempty"`
+	UrlToImage           string         `protobuf:"bytes,6,opt,name=urlToImage,proto3" json:"urlToImage,omitempty"`
+	PublishedAt          string         `protobuf:"bytes,7,opt,name=publishedAt,proto3" json:"publishedAt,omitempty"`
+	Content              string         `protobuf:"bytes,8,opt,name=content,proto3" json:"content,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
+	XXX_unrecognized     []byte         `json:"-"`
+	XXX_sizecache        int32          `json:"-"`
 }
 
 func (m *Article) Reset()         { *m = Article{} }
@@ -114,7 +116,7 @@ func (m *Article) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Article proto.InternalMessageInfo
 
-func (m *Article) GetSource() *Source {
+func (m *Article) GetSource() *ArticleSource {
 	if m != nil {
 		return m.Source
 	}
@@ -170,7 +172,7 @@ func (m *Article) GetContent() string {
 	return ""
 }
 
-type Source struct {
+type ArticleSource struct {
 	Id                   int32    `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Name                 string   `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -178,73 +180,220 @@ type Source struct {
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *Source) Reset()         { *m = Source{} }
-func (m *Source) String() string { return proto.CompactTextString(m) }
-func (*Source) ProtoMessage()    {}
-func (*Source) Descriptor() ([]byte, []int) {
+func (m *ArticleSource) Reset()         { *m = ArticleSource{} }
+func (m *ArticleSource) String() string { return proto.CompactTextString(m) }
+func (*ArticleSource) ProtoMessage()    {}
+func (*ArticleSource) Descriptor() ([]byte, []int) {
 	return fileDescriptor_5c593d380f9840a2, []int{2}
 }
 
-func (m *Source) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Source.Unmarshal(m, b)
+func (m *ArticleSource) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ArticleSource.Unmarshal(m, b)
 }
-func (m *Source) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Source.Marshal(b, m, deterministic)
+func (m *ArticleSource) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ArticleSource.Marshal(b, m, deterministic)
 }
-func (m *Source) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Source.Merge(m, src)
+func (m *ArticleSource) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ArticleSource.Merge(m, src)
 }
-func (m *Source) XXX_Size() int {
-	return xxx_messageInfo_Source.Size(m)
+func (m *ArticleSource) XXX_Size() int {
+	return xxx_messageInfo_ArticleSource.Size(m)
 }
-func (m *Source) XXX_DiscardUnknown() {
-	xxx_messageInfo_Source.DiscardUnknown(m)
+func (m *ArticleSource) XXX_DiscardUnknown() {
+	xxx_messageInfo_ArticleSource.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Source proto.InternalMessageInfo
+var xxx_messageInfo_ArticleSource proto.InternalMessageInfo
 
-func (m *Source) GetId() int32 {
+func (m *ArticleSource) GetId() int32 {
 	if m != nil {
 		return m.Id
 	}
 	return 0
 }
 
-func (m *Source) GetName() string {
+func (m *ArticleSource) GetName() string {
 	if m != nil {
 		return m.Name
 	}
 	return ""
 }
 
+type ArticlePagination struct {
+	Page                 int32    `protobuf:"varint,1,opt,name=page,proto3" json:"page,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ArticlePagination) Reset()         { *m = ArticlePagination{} }
+func (m *ArticlePagination) String() string { return proto.CompactTextString(m) }
+func (*ArticlePagination) ProtoMessage()    {}
+func (*ArticlePagination) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5c593d380f9840a2, []int{3}
+}
+
+func (m *ArticlePagination) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ArticlePagination.Unmarshal(m, b)
+}
+func (m *ArticlePagination) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ArticlePagination.Marshal(b, m, deterministic)
+}
+func (m *ArticlePagination) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ArticlePagination.Merge(m, src)
+}
+func (m *ArticlePagination) XXX_Size() int {
+	return xxx_messageInfo_ArticlePagination.Size(m)
+}
+func (m *ArticlePagination) XXX_DiscardUnknown() {
+	xxx_messageInfo_ArticlePagination.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ArticlePagination proto.InternalMessageInfo
+
+func (m *ArticlePagination) GetPage() int32 {
+	if m != nil {
+		return m.Page
+	}
+	return 0
+}
+
 func init() {
 	proto.RegisterType((*Articles)(nil), "proto.Articles")
 	proto.RegisterType((*Article)(nil), "proto.Article")
-	proto.RegisterType((*Source)(nil), "proto.Source")
+	proto.RegisterType((*ArticleSource)(nil), "proto.ArticleSource")
+	proto.RegisterType((*ArticlePagination)(nil), "proto.ArticlePagination")
 }
 
 func init() { proto.RegisterFile("article.proto", fileDescriptor_5c593d380f9840a2) }
 
 var fileDescriptor_5c593d380f9840a2 = []byte{
-	// 313 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x54, 0x90, 0x4f, 0x4b, 0x33, 0x31,
-	0x10, 0xc6, 0xdf, 0x76, 0xbb, 0xdb, 0xbe, 0xd3, 0x3f, 0xca, 0x20, 0x12, 0x7a, 0x90, 0xb2, 0xa0,
-	0x14, 0x91, 0x1e, 0xea, 0xc5, 0x6b, 0x0f, 0x22, 0x5e, 0xb7, 0x7e, 0x81, 0x74, 0x77, 0x68, 0x03,
-	0xe9, 0x66, 0x49, 0x26, 0x82, 0x5f, 0xda, 0xcf, 0x20, 0x4d, 0xa2, 0xb4, 0xa7, 0xcc, 0xf3, 0x7b,
-	0x92, 0x79, 0x32, 0x03, 0x53, 0x69, 0x59, 0xd5, 0x9a, 0x56, 0x9d, 0x35, 0x6c, 0x30, 0x0f, 0xc7,
-	0x7c, 0xba, 0xa7, 0x96, 0xac, 0xd4, 0x91, 0x96, 0x16, 0x46, 0x9b, 0x78, 0xcd, 0xe1, 0x2d, 0x14,
-	0x8e, 0x25, 0x7b, 0x27, 0x7a, 0x8b, 0xde, 0xf2, 0x7f, 0x95, 0x14, 0x96, 0x30, 0x61, 0xc3, 0x52,
-	0x57, 0xe4, 0xbc, 0x66, 0x27, 0xfa, 0xc1, 0xbd, 0x60, 0xf8, 0x08, 0xa3, 0x14, 0xe7, 0x44, 0xb6,
-	0xc8, 0x96, 0xe3, 0xf5, 0x2c, 0x26, 0xac, 0x52, 0xfb, 0xea, 0xcf, 0x2f, 0xbf, 0x7b, 0x30, 0x4c,
-	0x14, 0xef, 0xa1, 0x70, 0xc6, 0xdb, 0x9a, 0x42, 0xe6, 0x78, 0x3d, 0x4d, 0xaf, 0xb6, 0x01, 0x56,
-	0xc9, 0x3c, 0x7d, 0x4d, 0x7a, 0x3e, 0x18, 0x9b, 0xc2, 0x93, 0xc2, 0x1b, 0xc8, 0x59, 0xb1, 0x26,
-	0x91, 0x05, 0x1c, 0x05, 0x2e, 0x60, 0xdc, 0x90, 0xab, 0xad, 0xea, 0x58, 0x99, 0x56, 0x0c, 0x82,
-	0x77, 0x8e, 0xf0, 0x1a, 0x32, 0x6f, 0xb5, 0xc8, 0x83, 0x73, 0x2a, 0xf1, 0x0e, 0xc0, 0x5b, 0xfd,
-	0x61, 0xde, 0x8f, 0x72, 0x4f, 0xa2, 0x08, 0xc6, 0x19, 0x39, 0xf5, 0xec, 0xfc, 0x4e, 0x2b, 0x77,
-	0xa0, 0x66, 0xc3, 0x62, 0x18, 0x7b, 0x9e, 0x21, 0x14, 0x30, 0xac, 0x4d, 0xcb, 0xd4, 0xb2, 0x18,
-	0x05, 0xf7, 0x57, 0x96, 0x4f, 0x50, 0xc4, 0x79, 0x70, 0x06, 0x7d, 0xd5, 0x84, 0x51, 0xf3, 0xaa,
-	0xaf, 0x1a, 0x44, 0x18, 0xb4, 0xf2, 0x48, 0x69, 0xaa, 0x50, 0xaf, 0x5f, 0x60, 0x96, 0xb6, 0xb3,
-	0x25, 0xfb, 0xa9, 0x6a, 0xc2, 0x07, 0xc8, 0xde, 0x88, 0x71, 0x92, 0x76, 0xf3, 0x7a, 0xec, 0xf8,
-	0x6b, 0x7e, 0x75, 0xb9, 0x5f, 0x57, 0xfe, 0xdb, 0x15, 0x81, 0x3c, 0xff, 0x04, 0x00, 0x00, 0xff,
-	0xff, 0x17, 0x8b, 0x28, 0x05, 0xfa, 0x01, 0x00, 0x00,
+	// 350 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x51, 0xcd, 0x4e, 0xf3, 0x30,
+	0x10, 0x6c, 0x9b, 0x26, 0xed, 0xb7, 0xfd, 0xf9, 0x60, 0x55, 0x21, 0xab, 0x07, 0x54, 0xe5, 0x42,
+	0x85, 0x50, 0x0f, 0xe9, 0x13, 0x14, 0x09, 0x21, 0x6e, 0x28, 0xf0, 0x02, 0x6e, 0x62, 0xb5, 0x56,
+	0x9d, 0x38, 0xb2, 0x37, 0x48, 0x3c, 0x38, 0x77, 0x14, 0xc7, 0x85, 0x86, 0x53, 0x76, 0x66, 0xbc,
+	0xb3, 0xb3, 0x59, 0x98, 0x71, 0x43, 0x32, 0x53, 0x62, 0x53, 0x19, 0x4d, 0x1a, 0x43, 0xf7, 0x59,
+	0xce, 0x0e, 0xa2, 0x14, 0x86, 0xab, 0x96, 0x8d, 0x0d, 0x8c, 0x77, 0xed, 0x33, 0x8b, 0x37, 0x10,
+	0x59, 0xe2, 0x54, 0x5b, 0xd6, 0x5f, 0xf5, 0xd7, 0xff, 0x52, 0x8f, 0x30, 0x86, 0x29, 0x69, 0xe2,
+	0x2a, 0x15, 0xb6, 0x56, 0x64, 0xd9, 0xc0, 0xa9, 0x1d, 0x0e, 0xef, 0x61, 0xec, 0xc7, 0x59, 0x16,
+	0xac, 0x82, 0xf5, 0x24, 0x99, 0xb7, 0x13, 0x36, 0xde, 0x3e, 0xfd, 0xd1, 0xe3, 0xaf, 0x3e, 0x8c,
+	0x3c, 0x8b, 0x0f, 0x10, 0x59, 0x5d, 0x9b, 0x4c, 0xb8, 0x99, 0x93, 0x64, 0xd1, 0xed, 0x7a, 0x73,
+	0x5a, 0xea, 0xdf, 0x34, 0x09, 0x79, 0x4d, 0x47, 0x6d, 0x7c, 0x06, 0x8f, 0x70, 0x01, 0x21, 0x49,
+	0x52, 0x82, 0x05, 0x8e, 0x6e, 0x01, 0xae, 0x60, 0x92, 0x0b, 0x9b, 0x19, 0x59, 0x91, 0xd4, 0x25,
+	0x1b, 0x3a, 0xed, 0x92, 0xc2, 0x2b, 0x08, 0x6a, 0xa3, 0x58, 0xe8, 0x94, 0xa6, 0xc4, 0x5b, 0x80,
+	0xda, 0xa8, 0x77, 0xfd, 0x52, 0xf0, 0x83, 0x60, 0x91, 0x13, 0x2e, 0x98, 0xc6, 0xb3, 0xaa, 0xf7,
+	0x4a, 0xda, 0xa3, 0xc8, 0x77, 0xc4, 0x46, 0xad, 0xe7, 0x05, 0x85, 0x0c, 0x46, 0x99, 0x2e, 0x49,
+	0x94, 0xc4, 0xc6, 0x4e, 0x3d, 0xc3, 0x78, 0x0b, 0xb3, 0xce, 0x5a, 0x38, 0x87, 0x81, 0xcc, 0xdd,
+	0xe2, 0x61, 0x3a, 0x90, 0x39, 0x22, 0x0c, 0x4b, 0x5e, 0x08, 0xbf, 0x9c, 0xab, 0xe3, 0x3b, 0xb8,
+	0xf6, 0x4d, 0xaf, 0xfc, 0x20, 0x4b, 0xee, 0x72, 0x23, 0x0c, 0xab, 0x26, 0x5f, 0xdb, 0xea, 0xea,
+	0xa4, 0x82, 0xf9, 0xd9, 0x5d, 0x98, 0x0f, 0x99, 0x09, 0x4c, 0x20, 0x78, 0x16, 0x84, 0xac, 0xfb,
+	0x4b, 0x7f, 0x6d, 0x96, 0xff, 0xbb, 0x8a, 0x8d, 0x7b, 0xcd, 0x1d, 0x1f, 0xb5, 0x3e, 0x15, 0xdc,
+	0x9c, 0xf0, 0xcf, 0x05, 0x97, 0x53, 0x8f, 0x9f, 0x8a, 0x8a, 0x3e, 0xe3, 0xde, 0x3e, 0x72, 0x70,
+	0xfb, 0x1d, 0x00, 0x00, 0xff, 0xff, 0x1d, 0xd5, 0x9a, 0x15, 0x69, 0x02, 0x00, 0x00,
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConn
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion4
+
+// ArticleServiceClient is the client API for ArticleService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type ArticleServiceClient interface {
+	Get(ctx context.Context, in *ArticlePagination, opts ...grpc.CallOption) (*Articles, error)
+	Bookmark(ctx context.Context, in *Article, opts ...grpc.CallOption) (*Empty, error)
+}
+
+type articleServiceClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewArticleServiceClient(cc *grpc.ClientConn) ArticleServiceClient {
+	return &articleServiceClient{cc}
+}
+
+func (c *articleServiceClient) Get(ctx context.Context, in *ArticlePagination, opts ...grpc.CallOption) (*Articles, error) {
+	out := new(Articles)
+	err := c.cc.Invoke(ctx, "/proto.ArticleService/Get", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *articleServiceClient) Bookmark(ctx context.Context, in *Article, opts ...grpc.CallOption) (*Empty, error) {
+	out := new(Empty)
+	err := c.cc.Invoke(ctx, "/proto.ArticleService/Bookmark", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// ArticleServiceServer is the server API for ArticleService service.
+type ArticleServiceServer interface {
+	Get(context.Context, *ArticlePagination) (*Articles, error)
+	Bookmark(context.Context, *Article) (*Empty, error)
+}
+
+func RegisterArticleServiceServer(s *grpc.Server, srv ArticleServiceServer) {
+	s.RegisterService(&_ArticleService_serviceDesc, srv)
+}
+
+func _ArticleService_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ArticlePagination)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ArticleServiceServer).Get(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.ArticleService/Get",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ArticleServiceServer).Get(ctx, req.(*ArticlePagination))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ArticleService_Bookmark_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Article)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ArticleServiceServer).Bookmark(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.ArticleService/Bookmark",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ArticleServiceServer).Bookmark(ctx, req.(*Article))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _ArticleService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "proto.ArticleService",
+	HandlerType: (*ArticleServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Get",
+			Handler:    _ArticleService_Get_Handler,
+		},
+		{
+			MethodName: "Bookmark",
+			Handler:    _ArticleService_Bookmark_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "article.proto",
 }
