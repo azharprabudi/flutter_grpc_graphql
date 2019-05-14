2279789f3dafc25ae16a02b44062350ee4726fee
azharprabudi <azharprabui@gmail.com>
2019-05-14 13:38:35 +0700

    apply chat service

--- grpc_server/internal/grpc_server/utils/register_services.go
+++ grpc_server/internal/grpc_server/utils/register_services.go
@@ -1,15 +1,19 @@
 package utils
 
 import (
-	presentation "github.com/azharprabudi/flutter_grpc_graphql/grpc_server/internal/grpc_server/presentation/articles"
+	p1 "github.com/azharprabudi/flutter_grpc_graphql/grpc_server/internal/grpc_server/presentation/articles"
+	p2 "github.com/azharprabudi/flutter_grpc_graphql/grpc_server/internal/grpc_server/presentation/chat"
 	"github.com/azharprabudi/flutter_grpc_graphql/grpc_server/pkg/api/v1/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func RegisterServices(server *grpc.Server) {
-	article := presentation.NewArticlePresentation()
+	article := p1.NewArticlePresentation()
 	proto.RegisterArticleServiceServer(server, article)
+
+	chat := p2.NewChatPresentation()
+	proto.RegisterChatServiceServer(server, chat)
 
 	reflection.Register(server)
 }
