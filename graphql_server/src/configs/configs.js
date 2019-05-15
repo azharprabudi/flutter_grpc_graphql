97573467ead391cadadd1518b72e8ca0524952a2
Azhar Prabudi <azharprabui@gmail.com>
2019-05-16 01:18:17 +0700

    add config grpc address

--- graphql_server/src/configs/configs.js
+++ graphql_server/src/configs/configs.js
@@ -3,7 +3,8 @@ const configs = {
   FGG_REDIS_HOST: process.env.FGG_REDIS_HOST || "localhost",
   FGG_REDIS_PORT: process.env.FGG_REDIS_PORT || 6379,
   FGG_NEWS_API_KEY: process.env.FGG_NEWS_API_KEY || "rahasia",
-  FGG_GRAPHQL_PORT: process.env.FGG_GRAPHQL_PORT || 9669
+  FGG_GRAPHQL_PORT: process.env.FGG_GRAPHQL_PORT || 9669,
+  FGG_GRPC_ADDRESS: process.env.FGG_GRPC_ADDRESS || "http://127.0.0.1:6969"
 };
 
 module.exports = configs;
