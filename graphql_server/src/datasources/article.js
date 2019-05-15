592b29a8df541fd4deee713a087280fcd57ed8ab
azharprabudi <azharprabui@gmail.com>
2019-05-15 11:03:54 +0700

    add article data source

---
+++ graphql_server/src/datasources/article.js
@@ -0,0 +1,25 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+const DataLoader = require("dataloader");
+
+class ArticleAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://newsapi.org/v2";
+  }
+
+  progressLoader = new DataLoader(async page => {
+    const resp = await this.get("/top-headlines", {
+      country: "id",
+      category: "sports",
+      apiKey: "c6f23162e2f8454f8a87763a5c9d7fca"
+    });
+
+    return resp.data;
+  });
+
+  getArticle(page) {
+    return this.progressLoader.load(page);
+  }
+}
+
+module.exports = ArticleAPI;
