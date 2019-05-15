91484576427206efa07cd4cc59389af367eb50ed
Azhar Prabudi <azharprabui@gmail.com>
2019-05-16 02:02:58 +0700

    add endpoint post

--- graphql_server/src/configs/configs.js
+++ graphql_server/src/configs/configs.js
@@ -4,7 +4,9 @@ const configs = {
   FGG_REDIS_PORT: process.env.FGG_REDIS_PORT || 6379,
   FGG_NEWS_API_KEY: process.env.FGG_NEWS_API_KEY || "rahasia",
   FGG_GRAPHQL_PORT: process.env.FGG_GRAPHQL_PORT || 9669,
-  FGG_GRPC_ADDRESS: process.env.FGG_GRPC_ADDRESS || "localhost:6969"
+  FGG_GRPC_ADDRESS: process.env.FGG_GRPC_ADDRESS || "localhost:6969",
+  FGG_POST_BASEURL:
+    process.env.FGG_POST_BASEURL || "http://jsonplaceholder.typicode.com"
 };
 
 module.exports = configs;
