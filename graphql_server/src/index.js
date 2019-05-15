d4900bb01acfee1e35a804855dbb004bdf252bed
azharprabudi <azharprabui@gmail.com>
2019-05-15 11:11:05 +0700

    add datasource at main and redis cache

--- graphql_server/src/index.js
+++ graphql_server/src/index.js
@@ -1,5 +1,6 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const { makeExecutableSchema } = require("graphql-tools");
+const { RedisCache } = require("apollo-server-cache-redis");
 
 // typedef
 const QueryType = require("./types/query");
@@ -8,12 +9,22 @@ const ArticleType = require("./types/article");
 // resolver
 const QueryResolver = require("./resolvers/query");
 
+// datasource
+const ArticleAPI = require("./datasources/article");
+
 const server = new ApolloServer({
   schema: makeExecutableSchema({
     typeDefs: [QueryType, ArticleType],
     resolvers: QueryResolver
   }),
-  playground: process.env.NODE_ENV != "production"
+  cache: new RedisCache({
+    host: "127.0.0.1",
+    port: 6379
+  }),
+  playground: process.env.NODE_ENV != "production",
+  dataSources: () => ({
+    articleAPI: new ArticleAPI()
+  })
 });
 
 server
